5f816c77ed8dac743816589f5fe3b79af844b284
MeszarosGergo1 <meszaros.gergo@diak.szbi-pg.hu>
2025-09-15 14:56:14 +0200

    class kesz

--- MG-gokart/Program.cs
+++ MG-gokart/Program.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace MG_gokart
 {
+    class gokart
+    {
+        // (vezetéknév, keresztnévnév, születési idő, 18+(i/N), versenyzo azonosito, emailcim)
+        public string vnev;
+        public string knev;
+        public DateTime szido;
+        public bool felnotte;
+        public int vazonosito;
+        public string emailcim;
+        public gokart(string vnev, string knev, DateTime szido, bool felnotte, int vazonosito, string emailcim)
+        {
+            this.vnev = vnev;
+            this.knev = knev;
+            this.szido = szido;
+            this.felnotte = felnotte;
+            this.vazonosito = vazonosito;
+            this.emailcim = emailcim;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
