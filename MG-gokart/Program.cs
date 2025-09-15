c172a85c39117e77b2d077441e8e2e9c41f3bdcc
MeszarosGergo1 <meszaros.gergo@diak.szbi-pg.hu>
2025-09-15 14:40:07 +0200

    asdasd

--- MG-gokart/Program.cs
+++ MG-gokart/Program.cs
@@ -10,6 +10,25 @@ namespace MG_gokart
     {
         static void Main(string[] args)
         {
+
+            /*
+            Bevezető
+            MG 2025.09.15
+            */
+            string fejlec = "Bevezető";
+            Console.WriteLine(fejlec);
+
+            for (int i = 0; i < fejlec.Length; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine();
+            Console.WriteLine("Nyomja meg az ENTER-t a kilépéshez");
+            Console.ReadLine();
+
         }
     }
 }
