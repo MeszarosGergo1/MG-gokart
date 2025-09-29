a4f71e365cc157d72cc9ef50b5abbc5ad78f976f
MeszarosGergo1 <meszaros.gergo@diak.szbi-pg.hu>
2025-09-29 13:53:55 +0200

    adatok keszen

--- MG-gokart/Program.cs
+++ MG-gokart/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Collections;
-using System.Net.Http.Headers;
 
 namespace MG_gokart
 {
@@ -16,9 +17,9 @@ namespace MG_gokart
         public string knev;
         public DateTime szido;
         public bool felnotte;
-        public int vazonosito;
+        public string vazonosito;
         public string emailcim;
-        public gokart(string vnev, string knev, DateTime szido, bool felnotte, int vazonosito, string emailcim)
+        public gokart(string vnev, string knev, DateTime szido, bool felnotte, string vazonosito, string emailcim)
         {
             this.vnev = vnev;
             this.knev = knev;
@@ -30,6 +31,20 @@ namespace MG_gokart
     }
     internal class Program
     {
+        public static string Ekezetmentesit(string input)
+        {
+            string normalized = input.Normalize(NormalizationForm.FormD);
+            var sb = new StringBuilder();
+            foreach (char c in normalized)
+            {
+                UnicodeCategory uc = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (uc != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString().Normalize(NormalizationForm.FormC);
+        }
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -48,8 +63,14 @@ namespace MG_gokart
             List<gokart> versenyzok = new List<gokart>();
             Dictionary<DateTime, ArrayList> idopontok = new Dictionary<DateTime, ArrayList>();
             List<string> palya = new List<string>() { "天井-GoKart", "8879 Kerkateskánd, Csavargyár utca 56.",  "0692697752", "jovalasztas.hu"};
-            string[] keresztnev  = File.ReadAllLines("../../keresztnevek.txt");
-            string[] vezeteknev  = File.ReadAllLines("../../vezeteknevek.txt");
+            StreamReader knev = new StreamReader("../../keresztnevek.txt");
+            StreamReader vnev = new StreamReader("../../vezeteknevek.txt");
+            List<string> keresztnev  = new List<string>();
+            List<string> vezeteknev  = new List<string>();
+            string knevline = knev.ReadLine();
+            keresztnev = knevline.Split(',').ToList();
+            string vnevline = vnev.ReadLine();
+            vezeteknev = vnevline.Split(',').ToList();
             DateTime now = DateTime.Now;
             DateTime idopont = new DateTime(now.Year, now.Month, now.Day, 08, 00, 00);
             Random rnd = new Random();
@@ -68,15 +89,20 @@ namespace MG_gokart
             for (int i = 0; i < rnd.Next(1,151); i++)
             {
                 int honap = rnd.Next(1, 13);
-                DateTime ev = new DateTime(rnd.Next(1925, 2026));
+                DateTime ev = new DateTime(rnd.Next(1925, 2026), 01, 01);
                 DateTime szulido = new DateTime(ev.Year, honap, rnd.Next(1, DateTime.DaysInMonth(ev.Year, honap)));
+                Console.WriteLine(szulido);
                 bool felnotte = (DateTime.Now - szulido).TotalDays >= 18;
-                //folyamatban lévő munka az ez alatt lévő sorban
-                //versenyzok.Add(new gokart(vezeteknev[rnd.Next(0, vezeteknev.Length)], keresztnev[rnd.Next(0, keresztnev.Length)], szulido, felnotte, $"GO{}"));
+                string mentesknev = Ekezetmentesit(keresztnev[rnd.Next(0, keresztnev.Count)]).Replace("'", "").Trim();
+                string mentesvnev = Ekezetmentesit(vezeteknev[rnd.Next(0, vezeteknev.Count)]).Replace("'", "").Trim();
+                versenyzok.Add(new gokart(mentesvnev, mentesknev, szulido, felnotte, $"GO-{mentesvnev}{mentesknev}-{Convert.ToString(szulido.Year)}{Convert.ToString(szulido.Month)}{Convert.ToString(szulido.Day)}", $"{(mentesvnev+"."+mentesknev).ToLower()}@gmail.com"));
             }
 
-            Console.WriteLine();
-
+            foreach (var item in versenyzok)
+            {
+                Console.WriteLine(item.vazonosito);
+            }
+            
             Console.WriteLine();
             Console.WriteLine("Nyomja meg az ENTER-t a kilépéshez");
             Console.ReadLine();
